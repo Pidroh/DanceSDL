2a28f4738eb960a1a9d856bbd19ef996442cf871
Awennor <forworksfr@hotmail.com>
2020-03-07 11:57:09 +0900

    HTML size stuff

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"start": 0, "audio": 0, "end": 153152, "filename": "/assets/Oswald.ttf"}, {"start": 153152, "audio": 0, "end": 171505, "filename": "/assets/titleimg.png"}, {"start": 171505, "audio": 0, "end": 171705, "filename": "/assets/wsq.png"}], "remote_package_size": 171705, "package_uuid": "0915764e-750f-4647-bdd8-7fe43e79e1f0"});
+   loadPackage({"files": [{"start": 0, "audio": 0, "end": 153152, "filename": "/assets/Oswald.ttf"}, {"start": 153152, "audio": 0, "end": 171505, "filename": "/assets/titleimg.png"}, {"start": 171505, "audio": 0, "end": 171705, "filename": "/assets/wsq.png"}], "remote_package_size": 171705, "package_uuid": "f354769a-e149-4c89-9c64-ee3c02f49e58"});
   
   })();
   
